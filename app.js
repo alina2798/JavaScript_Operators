7238004f7a0ddefb536605643d14348888667431
“Alina <alinashulga27@gmail.com>
2024-08-03 16:46:02 -0400

    Updated

--- app.js
+++ app.js
@@ -5,15 +5,46 @@ console.log(
 
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
-
-// YOUR CODE HERE
+let a = 20;
+console.log(a);
+let b = 4;
+console.log(b);
+let sum = a + b;
+console.log(sum);
+let minus = a - b;
+console.log(minus);
+let multiply = a * b;
+console.log(multiply);
+let dividing = a / b;
+console.log(dividing);
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
-
-// YOUR CODE HERE
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+console.log(num + str);
+console.log(num + str2);
+console.log(num + isPresent);
+console.log(firstName + num);
+console.log(isPresent + str);
+console.log(firstName + lastName);
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
-
-// YOUR CODE HERE
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+console.log(val == str3);
+console.log(val === str3);
+console.log(!isPresent2);
+console.log("eleven" == str4 && val >= str3);
+console.log(!isPresent2 || isPresent2);
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 != false);
+console.log(0 !== false);
